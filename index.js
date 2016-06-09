904509a5eb66f813436317d5eca938e00619de3b
Bryan Jennings <bryanjenningz@gmail.com>
2016-06-08 21:54:44 -0700

    fix default browser character offset problem

--- index.js
+++ index.js
@@ -2,19 +2,44 @@ var textContainer = document.querySelector('#text')
 var popup = document.querySelector('#popup')
 var button = document.querySelector('button')
 var input = document.querySelector('input')
+var characters = []
+var text
 
 var displayTranslation = ({word, translations}) => {
   popup.innerHTML = 'word: ' + word + ', translation: ' + translations.join(', ')
 }
 
 button.addEventListener('click', e => {
-  textContainer.textContent = input.value
+  text = input.value
   input.value = ''
+  textContainer.textContent = ''
+
+  text.split('').map(ch => {
+    var el = document.createElement('span')
+    el.textContent = ch
+    return el
+  }).forEach((el, i) => {
+    textContainer.appendChild(el)
+    var sides = el.getClientRects()[0]
+    characters.push(Object.assign(
+      {}, 
+      {left: sides.left, right: sides.right, top: sides.top, bottom: sides.bottom},
+      {text: el.textContent, index: i}
+    ))
+  })
 })
 
 textContainer.addEventListener('click', e => {
-  var offsetIndex = document.caretRangeFromPoint(e.clientX, e.clientY).startOffset
-  var text = textContainer.textContent
+  var clickedCharacter = characters.filter(ch => {
+    return ch.left < e.clientX && ch.right && e.clientX < ch.right && 
+      ch.top < e.clientY && e.clientY < ch.bottom 
+  })[0]
+
+  if (clickedCharacter === undefined) {
+    return
+  }
+
+  var offsetIndex = clickedCharacter.index
 
   // Check word definitions for the 10-character word, then 9, then 8, 7, ..., 2, 1
   for (var wordLength = 10; wordLength > 0; wordLength--) {
@@ -25,3 +50,4 @@ textContainer.addEventListener('click', e => {
     }
   }
 })
+
