90315f22f0893d556fe74eb9983d4e8a4f77b30e
Bryan Jennings <bryanjenningz@gmail.com>
2016-06-08 22:43:40 -0700

    fix scrolling

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ button.addEventListener('click', e => {
     el.textContent = ch
     return el
   }).forEach((el, i) => {
+    // The reason why I'm appending each element to the DOM is so that I can get
+    // the character's x-position, which is more accurate than the offsetIndex 
+    // method. Sadly, the y-position isn't accurate for this method because it doesn't
+    // take into consideration the scrollY value, and the scrollY value isn't compatible
+    // across all browsers, so I have to just compare the offsetIndex to be able to
+    // determine which character gets touched by the user.
     textContainer.appendChild(el)
     var sides = el.getClientRects()[0]
     characters.push(Object.assign(
@@ -27,12 +33,21 @@ button.addEventListener('click', e => {
       {text: el.textContent, index: i}
     ))
   })
+
+  textContainer.textContent = text
 })
 
 textContainer.addEventListener('click', e => {
+  // offsetIndex isn't a reliable source for determining the character, since it 
+  // tends to give the character to the right if you click the right side of a character.
+  // To fix this, I'm going to use e.clientX, which gives a more accurate x-value,
+  // then we're going to filter for characters that are within 1 of offsetIndex.
+  var offsetIndex = document.caretRangeFromPoint(e.clientX, e.clientY).startOffset
+
   var clickedCharacter = characters.filter(ch => {
-    return ch.left < e.clientX && ch.right && e.clientX < ch.right && 
-      ch.top < e.clientY && e.clientY < ch.bottom 
+    return ch.left < e.clientX && ch.right && e.clientX < ch.right
+  }).filter(ch => {
+    return Math.abs(ch.index - offsetIndex) <= 1
   })[0]
 
   if (clickedCharacter === undefined) {
